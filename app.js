7581e75b3e20f9d3795fe9672aa9d38c1fa77bdc
BallisticSwami <sbahadkar@rocketmail.com>
2021-06-11 18:25:19 +0530

    Implemented para type with checks

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const path = require('path')
 const express = require('express')
 const nodeHtmlToImage = require('node-html-to-image')
 const myHtml = require('./docs.js')
+const font2b64 = require('node-font2base64')
+const fontDB = require('./fontDB')
+const { checkPrime } = require('crypto')
+const { get } = require('http')
 
 const app = express()
 const port = process.env.PORT || 3000
@@ -11,20 +15,151 @@ const publicDirPath = path.join(__dirname, '/html')
 app.use(express.static(publicDirPath))
 
 app.get('', (req, res) => {
-  res.send({error: 'emrror'})
+  res.send({ error: 'emrror' })
 })
 
 app.get('/gen', async (req, res) => {
   if (!req.query.img) {
     res.send({ error: 'img not provided' })
   } else {
-    const image = await nodeHtmlToImage({html: myHtml.htmlDoc, transparent: true})
-    res.writeHead(200, { 'Content-Type': 'image/png' });
-    res.end(image, 'binary');
+    console.log('Starting...')
+    if (req.query.img !== 'para') {
+      res.send({ error: 'img type invalid' })
+      return
+    }
+    if (!req.query.f1) {
+      req.query.f1 = 'Arial'
+    }
+    if (!req.query.f2) {
+      req.query.f2 = 'Arial'
+    }
+    if (!(checkFont(req.query.f1) && checkFont(req.query.f2))) {
+      res.send({ error: 'Invalid fontface' })
+      return
+    }
+    else {
+      console.log('Checks passed.')
+      const font1 = getFont(req.query.f1)
+      const font2 = getFont(req.query.f2)
+      const weight1 = getWeight1(font1, req.query.w1)
+      const weight2 = getWeight2(font2, req.query.w2)
+      var font1Url = ''
+      var font2Url = ''
+      if (font1.family === 'Arial') {
+        font1Url = 'undefined'
+      } else {
+        font1Url = font2b64.encodeToDataUrlSync('./fonts/' + font1.family + '/' + font1.family + '-' + weight1 + '.ttf')
+      }
+      if (font2.family === 'Arial') {
+        font2Url = 'undefined'
+      } else {
+        font2Url = font2b64.encodeToDataUrlSync('./fonts/' + font2.family + '/' + font2.family + '-' + weight2 + '.ttf')
+      }
+
+      const config = {
+        fontHead: {
+          family: font1.family,
+          fontUrl: font1Url,
+          weight: weight1
+        },
+        fontSub: {
+          family: font2.family,
+          fontUrl: font2Url,
+          weight: weight2
+        }
+      }
+      const image = await nodeHtmlToImage({ html: myHtml.htmlDoc(config), transparent: true })
+      res.writeHead(200, { 'Content-Type': 'image/png' });
+      res.end(image, 'binary');
+    }
   }
+
 })
 
 
 app.listen(port, () => {
   console.log('Server is up on port ' + port + '.');
 })
+
+
+function checkFont(font) {
+  if (font === 'Arial') {
+    return true
+  }
+  var i = fontDB.db.every(f => {
+    if (f.family === font) {
+      return true
+    }
+  })
+  if (i) {
+    return i
+  }
+  return false
+}
+
+function getFont(font) {
+  if (font === "Arial") {
+    return {
+      family: "Arial",
+      type: "Sans Serif",
+      weights: [400]
+    }
+  }
+  var i
+  fontDB.db.every(f => {
+    if (f.family === font) {
+      i = f
+    }
+  })
+  return i
+}
+
+
+function getWeight1(font, weight) {
+  weight = parseInt(weight)
+  if (font === "Arial") {
+    return 400
+  }
+  if (!weight) {
+    if (font.weights.includes(600)) {
+      return 600
+    } else if (font.weights.includes(400)) {
+      return 400
+    } else {
+      return font.weights[0]
+    }
+  }
+
+  if (font.weights.includes(weight)) {
+    return weight
+  } else if (font.weights.includes(600)) {
+    return 600
+  } else if (font.weights.includes(400)) {
+    return 400
+  } else {
+    return font.weights[0]
+  }
+}
+
+function getWeight2(font, weight) {
+  weight = parseInt(weight)
+  if (font === "Arial") {
+    return 400
+  }
+  if (!weight) {
+    if (font.weights.includes(400)) {
+      return 400
+    } else {
+      return font.weights[0]
+    }
+  }
+
+  if (font.weights.includes(weight)) {
+    return weight
+  } else if (font.weights.includes(400)) {
+    return 400
+  } else {
+    return font.weights[0]
+  }
+}
+
--- docs.js
+++ docs.js
@@ -1,59 +1,78 @@
-const htmlDoc = `
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
-  <style>#box {
-    width: 700px;
-    background-color: transparent;
-    padding: 35px 40px 40px 40px;
-  }
+const htmlDoc = function getPara(config) {
+  var doc = `
+  <!DOCTYPE html>
+  <html lang="en">
   
-  .head {
-    font-size: 80px;
-    font-weight: 600;
-  }
-  
-  .sub-div {
-    padding-left: 4px;
-  }
-  
-  .sub {
-    font-size: 32px;
-    font-weight: 400;
-  }
-  
-  button {
-    padding: 10px 30px 12px 25px;
-    font-size: 25px;
-    font-weight: 400;
-    border: none;
-    background: none;
-    outline: none;
-    color: black;
-    border: 2px solid;
-    border-radius:45px;
-  }
-  </style>
-</head>
-<body>
-  <div id="box">
-    <span class="head">Nice Headline Here</span><br><br>
-    <div class="sub-div">
-      <span class="sub">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been
-        the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and
-        scrambled it to make a type specimen book.</span>
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+    `+ getFontCSS(config.fontHead) + getFontCSS(config.fontSub) +`
+    #box {
+      width: 750px;
+      background-color: transparent;
+      padding: 35px 40px 40px 40px;
+    }
+    
+    .head {
+      font-size: 80px;
+      font-weight: ${config.fontHead.weight};
+      font-family: "${config.fontHead.family}";
+    }
+    
+    .sub-div {
+      padding-left: 4px;
+      padding-right: 40px;
+    }
+    
+    .sub {
+      font-size: 32px;
+      font-weight: ${config.fontSub.weight};
+      font-family: "${config.fontSub.family}";
+    }
+    
+    button {
+      padding: 10px 30px 12px 25px;
+      font-size: 25px;
+      font-weight: 400;
+      border: none;
+      background: none;
+      outline: none;
+      color: black;
+      border: 2px solid;
+      border-radius:45px;
+      font-family: "${config.fontSub.family}";
+    }
+    </style>
+  </head>
+  <body>
+    <div id="box">
+      <span class="head">Heart Surgeon</span><br><br>
+      <div class="sub-div">
+        <span class="sub">Number one. Steady hand. One day, Kim Jong Un need new heart. I do operation. But mistake! Kim Jong Un die! SSD very mad! I hide fishing boat, come to America. No English, no food, no money. Darryl give me job. Now I have house, American car and new woman. Darryl save life.
+        My big secret. I kill Kim Jong Un on purpose. I good surgeon. The best!</span>
+      </div>
+      <br><br>
+      <button>❤ &nbsp;&nbsp;Button</button>
     </div>
-    <br><br>
-    <button>❤ &nbsp;&nbsp;Button</button>
-  </div>
-</body>
+  </body>
+  
+  </html>
+  `
+  // console.log(doc)
+  return doc
+}
 
-</html>
-`
+function getFontCSS(font) {
+  return `@font-face {
+    font-family: '${font.family}';
+    font-style: normal;
+    font-weight: ${font.weight};
+    src: url(${font.fontUrl}) format('woff2');
+  }`
+}
+
 
 module.exports.htmlDoc = htmlDoc
